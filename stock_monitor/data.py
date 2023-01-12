2141bc802a7a95feb27aba8cfe05b16a17402eeb
Danil Petrov <danil.petrov@arrival.com>
2023-01-12 14:38:10 +0100

    Fix merge

--- stock_monitor/data.py
+++ stock_monitor/data.py
@@ -103,9 +103,9 @@ def ideas(period: str, interval: str):
                                   expected at \$3 and \$4. Easy double with \$4 fwd EPS and 10.5x multiple. DCF model
                                   results in \$87/share. **Exp. gain: +100% by 2H23.**
                                   [Source.](https://twitter.com/InvestSpecial/status/1612025171879010305)"""),
-<<<<<<< HEAD
-            Stock("XMTR", period=period, interval=interval,
-                  description=r"""Marketplace for small batch manufacturing and prototyping. Currently trades at its 
+             Stock("XMTR", period=period, interval=interval,
+                   expectation=Expectation(price=45, date=datetime(2023, 12, 31, tzinfo=timezone.utc)),
+                   description=r"""Marketplace for small batch manufacturing and prototyping. Currently trades at its
                                   Jun’21 IPO level and at 4.5x revenue, while comps are in the 1-2x range. Competitive 
                                   and low-entry barrier industry that is significantly exposed to macro headwinds. 
                                   Clients and suppliers can as easily connect directly. 
@@ -113,9 +113,7 @@ def ideas(period: str, interval: str):
                                   to drive traffic. Guidedown in pricing already happened with Q3’22 results, 
                                   volume might drop in 2023.Trades at 4.5x revenues, still at the IPO price vs. a comp 
                                   universe 3-D printers and short-run fabricators in the 1-2x range. **Exp. gain: 50%-75%**
-                                  [Source.](https://twitter.com/InvestSpecial/status/1612387567302840320)""")
-            ]
-=======
+                                  [Source.](https://twitter.com/InvestSpecial/status/1612387567302840320)"""),
              Stock("FIP", period=period, interval=interval,
                    expectation=Expectation(price=7, date=datetime(2023, 6, 1, tzinfo=timezone.utc)),
                    description=r"""Recent spin-off from \$FTAI with 4 infrastructure assets: 3 energy terminals and a
@@ -153,7 +151,6 @@ def ideas(period: str, interval: str):
                                   [Source](https://twitter.com/InvestSpecial/status/1613508259154984962)""")
              ]
     return sorted(ideas, key=lambda x: x.expectation.date)
->>>>>>> 9629f55 (Add new ideas)
 
 
 @cache(persist=False, ttl=3600, allow_output_mutation=True)
