2c3dc7f3b367f545c91fede14a5707b3588edb1f
d61h6k4 <ddbihbka@gmail.com>
2023-05-14 15:15:40 +0200

    Fix

--- stock_monitor/bin/monitor.py
+++ stock_monitor/bin/monitor.py
@@ -1,36 +1,38 @@
 import requests
+import json
 
 from tqdm import tqdm
 from stock_monitor.models import Stock
 from stock_monitor.data import ideas, stocks
 
-_BOT_URL = "https://lochaufwallstrasse.de/bot/conversations/d61h6k4/trigger_intent?output_channel=callback"
+_BOT_URL = "https://lochaufwallstrasse.de/bot/conversations/dbihbka/trigger_intent?output_channel=callback"
 
 
 def watch(stock):
-    r = requests.post(_BOT_URL, data={"name": "ask_set_watchlist_reminder",
-                                      "entities": {
-                                          "ticker": stock.ticker_name,
-                                          "seconds": 43200
-                                      }})
+    r = requests.post(_BOT_URL, data=json.dumps({"name": "ask_set_watchlist_reminder",
+                                                 "entities": {
+                                                     "ticker": stock.ticker_name,
+                                                     "seconds": 43200
+                                                 }}))
     r.raise_for_status()
 
 
 def unwatch(stock):
-    r = requests.post(_BOT_URL, data={"name": "ask_set_unwatchlist_reminder",
-                                      "entities": {
-                                          "ticker": stock.ticker_name,
-                                          "seconds": 43200
-                                      }})
+    r = requests.post(_BOT_URL, data=json.dumps({"name": "ask_set_unwatchlist_reminder",
+                                                 "entities": {
+                                                     "ticker": stock.ticker_name,
+                                                     "seconds": 43200
+                                                 }}))
     r.raise_for_status()
 
 
 def monitor(stock):
-    r = requests.post(_BOT_URL, data={"name": "ask_set_asr_reminder",
-                                      "entities": {
-                                          "ticker": stock.ticker_name,
-                                          "seconds": 3600
-                                      }})
+    r = requests.post(_BOT_URL, data=json.dumps({"name": "ask_set_asr_reminder",
+                                                 "entities": {
+                                                     "ticker": stock.ticker_name,
+                                                     "seconds": 3600
+                                                 }}))
+
     r.raise_for_status()
 
 
